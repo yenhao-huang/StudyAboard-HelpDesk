2f52bd7a1a159fcfd91f0e58959ce68882ab846c
root <blackpig22017@gmail.com>
2025-08-20 11:02:59 +0800

    Remove web frontend/backend, switch to Telegram UI

--- frontend/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import ChatbotUI from "./MultiTurnChat";
-import { sseProvider } from "./providers"; // 或 simpleProvider
-export default function App() {
-  return <ChatbotUI chatProvider={sseProvider} />;
-}
